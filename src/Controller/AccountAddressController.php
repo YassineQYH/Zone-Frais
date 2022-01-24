b3c0b442968f42578206f993929c662e18a5bea3
Qayouh <60608484+YassineQYH@users.noreply.github.com>
2022-01-24 13:25:35 +0100

    Update AccountAddressController.php

--- src/Controller/AccountAddressController.php
+++ src/Controller/AccountAddressController.php
@@ -36,10 +36,10 @@ class AccountAddressController extends AbstractController
     /**
      * @Route("/compte/ajouter-une-adresse", name="account_address_add")
      */
-    public function add(Cart $cart, Request $request)
+    public function add(Cart $cart, Request $request, CategoryRepository $category)
     {
         //dd($this->getUser());
-        
+        $categories = $category->findAll();
         $address = new Address();
         $form = $this->createForm(AddressType::class, $address);
 
@@ -57,15 +57,17 @@ class AccountAddressController extends AbstractController
         }
 
         return $this->render('account/address_form.html.twig', [
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'categories' => $categories
         ]);
     }
 
     /**
      * @Route("/compte/modifier-une-adresse/{id}", name="account_address_edit")
      */
-    public function edit(Request $request, $id)
+    public function edit(Request $request, $id, CategoryRepository $category)
     {
+        $categories = $category->findAll();
         $address = $this->entityManager->getRepository(Address::class)->findOneById($id);
 
         if (!$address || $address->getUser() != $this->getUser()) {
@@ -82,21 +84,25 @@ class AccountAddressController extends AbstractController
         }
 
         return $this->render('account/address_form.html.twig', [
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'categories' => $categories
         ]);
     }
 
     /**
      * @Route("/compte/supprimer-une-adresse/{id}", name="account_address_delete")
      */
-    public function delete($id)
+    public function delete($id, CategoryRepository $category)
     {
+        $categories = $category->findAll();
         $address = $this->entityManager->getRepository(Address::class)->findOneById($id);
 
         if ($address && $address->getUser() == $this->getUser()) {
             $this->entityManager->remove($address);
             $this->entityManager->flush();
         }
-            return $this->redirectToRoute('account_address');
+            return $this->redirectToRoute('account_address', [
+                'categories' => $categories
+            ]);
     }
 }
