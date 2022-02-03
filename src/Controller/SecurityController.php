a22a5412acaea7bd875bc6c93be1c5cc3851b503
YassineQYH <60608484+YassineQYH@users.noreply.github.com>
2022-02-03 23:17:51 +0100

    S'enregistrer ne fonctionne pas, test en cours

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -34,6 +34,21 @@ class SecurityController extends AbstractController
      */
     public function login(CategoryRepository $category, AuthenticationUtils $authenticationUtils, Request $request, UserPasswordEncoderInterface $encoder): Response
     {
+        $categories = $category->findAll();
+        
+        /* Login */
+        $category = $this->entityManager->getRepository(Category::class)->findAll();
+
+        /* if ($this->getUser()) {
+             return $this->redirectToRoute('account');
+         } */
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        
         /* Inscription */
         $notification = null;
 
@@ -44,42 +59,24 @@ class SecurityController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $user = $form->getData();
-
             $search_email = $this->entityManager->getRepository(User::class)->findOneByEmail($user->getEmail());
 
             if (!$search_email) {
                 $password = $encoder->encodePassword($user, $user->getPassword());
                 $user->setPassword($password);
-    
                 
                 $this->entityManager->persist($user);  /* Persister/Figer l'entité user et prépare toi à être créer en bdd car j'ai besoin de l'enregistrer */
                 $this->entityManager->flush(); /* Exécuter la persistance, l'enregistrer en bdd */
 
                 $mail = new Mail();
-                $content = "Bonjour ".$user->getFirstname()."</br>Bienvenue sur la première boutique dédié au made in France.</br></br>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias eum, consequuntur cum deserunt, nobis maxime quod cupiditate nulla maiores id nesciunt ipsam officia eos sunt minima sapiente voluptatum repellendus amet praesentium autem iure voluptatem veritatis atque perspiciatis? Dolor, eum voluptate eligendi, adipisci, eius et minima modi odio nostrum voluptates deserunt.
-                ";
+                $content = "Bonjour ".$user->getFirstname()."</br>Bienvenue sur la première boutique dédié au made in France.</br></br>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias eum, consequuntur cum deserunt, nobis maxime quod cupiditate nulla maiores id nesciunt ipsam officia eos sunt minima sapiente voluptatum repellendus amet praesentium autem iure voluptatem veritatis atque perspiciatis? Dolor, eum voluptate eligendi, adipisci, eius et minima modi odio nostrum voluptates deserunt.";
                 $mail->send($user->getEmail(), $user->getFirstname(), 'Bienvenue sur la Boutique SY-Shop', $content);
 
                 $notification = "Votre inscription s'est correctement déroulée. Vous pouvez dés à présent vous connecter à votre compte.";
             } else {
                 $notification = "L'email que vous avez renseigné existe déjà.";
             }
-
-
         }
-
-        /* Login */
-        $categories = $category->findAll();
-        $category = $this->entityManager->getRepository(Category::class)->findAll();
-
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername, 
